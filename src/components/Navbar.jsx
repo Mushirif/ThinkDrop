b53771a13ede7d2b7231c58e48843ae2556158b4
Mushirif Areekkan <mushirifareekkan@Mushirifs-MacBook-Air.local>
2025-07-10 14:04:34 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+import Image from "./Image";
+import { Link } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <Link to="/" className="flex items-center gap-4 text-2xl">
+        <Image src="logo.png" alt="ThinkDrop" w={32} h={32} />
+        <span>ThinkDrop</span>
+      </Link>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        {/* mobile button */}
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "="}
+        </div>
+        {/* mobile link list */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 absolute top-16 ${
+            open ? "-right-0" : "-right-[100%]"
+          } transition-all ease-in-out`}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </Link>
+        </div>
+      </div>
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most popular</Link>
+        <Link to="/">About</Link>
+
+        <SignedOut>
+          <Link to="/login">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
